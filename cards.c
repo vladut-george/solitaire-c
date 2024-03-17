c92d048bc6fdf416a26a1791933abe58c9ae6a9e
Buwubie <152532979+Buwubie@users.noreply.github.com>
2024-03-17 12:08:55 +0000

    header for the cards is complete, implementations to be written later

---
+++ cards.c
@@ -0,0 +1,5 @@
+//
+// Created by vladb on 17/03/2024.
+//
+
+#include "cards.h"
---
+++ cards.h
@@ -0,0 +1,32 @@
+//
+// Created by vladb on 17/03/2024.
+//
+
+#ifndef CARDS_H
+#define CARDS_H
+
+#include "utils.h"
+
+struct card {
+    char suit;
+    char number;
+    char display[3];
+};
+
+typedef struct card* deck;
+
+const char suits[] = {'c'/*clubs*/, 'd'/*diamonds*/, 'h'/*hearts*/, 's'/*spades*/};
+const unsigned int suits_l = 4;
+
+const char numbers[] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'};
+const unsigned int numbers_l = 13;
+
+
+struct card generateCard(char _suit, char _number);
+
+deck generateDeck(char _suits[], unsigned int _suits_l, char _numbers[], char _numbers_l);
+
+//_shuffle should be generate with generatePermutation of the length of the deck;
+deck shuffleDeck(deck _to_shuffle, permutation _shuffle);
+
+#endif //CARDS_H
